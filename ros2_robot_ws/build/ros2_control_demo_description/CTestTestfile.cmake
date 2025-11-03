4b3002410ef1fcea518544095ea583cb49ffeaa7
adelazapatera <adezaparg@gmail.com>
2025-11-03 18:24:30 +0100

    Add ROS 2 Jazzy robot demo (example_7 + description) with README and .gitignore

---
+++ ros2_robot_ws/build/ros2_control_demo_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/adeza/OneDrive/Escritorio/2MII/IngenieriaSoftware/SE25/ros2_robot_ws/src/ros2_control_demo_description
+# Build directory: /mnt/c/Users/adeza/OneDrive/Escritorio/2MII/IngenieriaSoftware/SE25/ros2_robot_ws/build/ros2_control_demo_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
